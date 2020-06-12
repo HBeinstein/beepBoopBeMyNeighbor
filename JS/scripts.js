5d68f3807c3185831e5a3b19d7b62ce56c6a05fa
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-12 13:12:17 -0700

    edit target area to append results in scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,13 +2,13 @@ function beepBoopOrNeighbor(num, name) {
   let results
   for(x=1; x<=num; x++) {
     if(x.toString().includes("3")) {
-      $("#resultsContainer").append(`<p>Won't you be my neighbor ${name}?</p>`);
+      $("#resultsList").append(`<p>Won't you be my neighbor ${name}?</p>`);
     } else if(x.toString().includes("2")) {
-      $("#resultsContainer").append("<p>Boop!</p>");
+      $("#resultsList").append("<p>Boop!</p>");
     } else if(x.toString().includes("1")) {
-      $("#resultsContainer").append("<p>Beep!</p>");
+      $("#resultsList").append("<p>Beep!</p>");
     } else {
-      $("#resultsContainer").append(`<p>${x}</p>`);
+      $("#resultsList").append(`<p>${x}</p>`);
     }
   }
 }
