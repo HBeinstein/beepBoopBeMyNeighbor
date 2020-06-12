c5fb979c33025adf1927a78dec21452a624e9c37
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-12 09:42:24 -0700

    add beepBoopOrNeighbor function to scripts.js to determine what word to print to page

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,22 @@
-$( document ).ready(function() {
-  $( "#" ).submit(function() {
-    
+function beepBoopOrNeighbor(num) {
+  for(x=1; x<=num; x++) {
+    if(x.toString().includes("3")) {
+      console.log("this has a three");
+    } else if(x.toString().includes("2")) {
+    console.log("This has a 2");
+    } else if(x.toString().includes("1")) {
+      console.log("This has a 1");
+    } else {
+      console.log(x);
+    }
+  }
+}
 
-    
+$( document ).ready(function() {
+  $("#userInputForm").submit(function() {
+    event.preventDefault()
+
+    const parsedUserInput = parseInt($("#userNumInput").val());
+    beepBoopOrNeighbor(parsedUserInput);
   });
 });
